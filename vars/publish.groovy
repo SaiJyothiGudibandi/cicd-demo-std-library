16d9df0dd38305f468142cb19cc2d480c0a4f2bf
SAIJYOTHI GUDIBANDI <saijyothi.gudibandi@broadcom.com>
2020-04-23 17:35:35 -0700

    test stage files

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,23 +1,5 @@
-def publishStages(){
-    def publishers = [:]
-    publishers["docker"] = {
-        stage("Build-Docker-Image") {
-            echo "docker build -t ${docker_img}:${docker_tag} ."
-        }
-        stage("Publish-Docker-Image-to-Artifactory") {
-            echo "Publish docker image - ${docker_img}:${docker_tag} to artifactory"
-        }
+def call(){
+    stage(publish) {
+        sh "echo 'publish stage"
     }
-    publishers["gcr"] = {
-        stage("Publish-Docker-Image-to-GCR") {
-            echo "Publishing docker image - ${docker_img}:${docker_tag} to GCR"
-        }
-    }
-    publishers["helm-chart"] = {
-        //Publish helm chart to artifact
-        stage("Publish-Helm-Chart-to-Artifactory") {
-            echo "Publish Helm Chart ${helm_chart_url} "
-        }
-    }
-    parallel publishers
 }
