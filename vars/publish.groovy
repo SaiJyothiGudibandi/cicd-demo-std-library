c4ac682e0aa5dd7c834770f84371b0c34e5ea4be
SAIJYOTHI GUDIBANDI <saijyothi.gudibandi@broadcom.com>
2020-04-23 19:25:03 -0700

    push-10

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -20,9 +20,12 @@ def executeBuildConfig(build_info) {
     def artifacts = []
     build_info.eachWithIndex { it, i ->
         stage(it["name"]) {
-            if(it["name"] == "docker" && it["image"].containsKey("name")){
-                echo("docker name ${it["image"]["name"]}")
-                echo("Executing ${it["name"]}")
+            if(it["name"] == "docker" && it["image"].containsKey("name") && it["image"].containsKey("tag")){
+                def docker_img = ${it["image"]["name"]}
+                def docker_tag = ${it["image"]["tag"]}
+                echo("docker name ${docker_img}")
+                echo("docker name ${docker_tag}")
+                echo "docker build -t ${docker_img}:${docker_tag} ."
                 echo "end exec"
             }
         }
