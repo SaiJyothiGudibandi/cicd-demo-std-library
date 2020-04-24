b8d17c68f2fab8db35d5ffd7d2e6b669283f3605
SAIJYOTHI GUDIBANDI <saijyothi.gudibandi@broadcom.com>
2020-04-23 19:57:08 -0700

    push-11

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -13,33 +13,34 @@ def call(body) {
             build_info = readYaml file: yaml_file
         }
         executeBuildConfig(build_info)
+
     }
 }
 def executeBuildConfig(build_info) {
     build_info.eachWithIndex { it, i ->
-            if(it["name"] == "docker" && it["image"].containsKey("name") && it["image"].containsKey("tag") && it["artifactory"].containsKey('url')){
-                stage("Build-Docker-Image") {
-                    def docker_img = it["image"]["name"]
-                    def docker_tag = it["image"]["tag"]
-                    echo "docker build -t ${docker_img}:${docker_tag} ."
-                }
-                stage("Publish-Docker-Image-To-Artifactory"){
-                    def docker_arti_url = it["artifactory"]["url"]
-                    echo "Publish Docker image to ${docker_arti_url}"
-                }
+        if(it["name"] == "docker" && it["image"].containsKey("name") && it["image"].containsKey("tag") && it["artifactory"].containsKey('url')){
+            stage("Build-Docker-Image") {
+                def docker_img = it["image"]["name"]
+                def docker_tag = it["image"]["tag"]
+                echo "docker build -t ${docker_img}:${docker_tag} ."
             }
-            if(it["name"] == "helm" && it["chart"].containsKey("name") && it["chart"].containsKey("version") && it["artifactory"].containsKey("url")){
-                stage("Build-Helm-Chart") {
-                    def helm_chart_name = it["chart"]["name"]
-                    def helm_chart_version = it["chart"]["version"]
-                    echo("Helm chart name ${helm_chart_name}")
-                    echo("Helm chart name ${helm_chart_version}")
-                    echo "Build Helm Chart ${helm_chart_name}:${helm_chart_version}"
-                }
-                stage("Publish-Helm-Chart-To-Artifactory") {
-                    def docker_arti_url = it["artifactory"]["url"]
-                    echo "Publish Helm Chart  to ${docker_arti_url}"
-                }
+            stage("Publish-Docker-Image-To-Artifactory"){
+                def docker_arti_url = it["artifactory"]["url"]
+                echo "Publish Docker image to ${docker_arti_url}"
             }
+        }
+        if(it["name"] == "helm" && it["chart"].containsKey("name") && it["chart"].containsKey("tag") && it["artifactory"].containsKey('url')){
+            stage("Build-Helm-Chart") {
+                def helm_chart_name = it["chart"]["name"]
+                def helm_chart_version = it["chart"]["version"]
+                echo("Helm chart name ${helm_chart_name}")
+                echo("Helm chart name ${helm_chart_version}")
+                echo "Build Helm Chart ${helm_chart_name}:${helm_chart_version}"
+            }
+            stage("Publish-Helm-Chart-To-Artifactory") {
+                def docker_arti_url = it["artifactory"]["url"]
+                echo "Publish Helm Chart  to ${docker_arti_url}"
+            }
+        }
     }
 }
