74657fa99b59e2f3dfbd2307531dec432abca0a5
SAIJYOTHI GUDIBANDI <saijyothi.gudibandi@broadcom.com>
2020-04-23 19:40:45 -0700

    push-11

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -17,17 +17,32 @@ def call(body) {
     }
 }
 def executeBuildConfig(build_info) {
-    def artifacts = []
     build_info.eachWithIndex { it, i ->
-        stage(it["name"]) {
-            if(it["name"] == "docker" && it["image"].containsKey("name") && it["image"].containsKey("tag")){
-                def docker_img = it["image"]["name"]
-                def docker_tag = it["image"]["tag"]
-                echo("docker name ${docker_img}")
-                echo("docker name ${docker_tag}")
-                echo "docker build -t ${docker_img}:${docker_tag} ."
-                echo "end exec"
+            if(it["name"] == "docker" && it["image"].containsKey("name") && it["image"].containsKey("tag") && it["artifactory"].containsKey('url')){
+                stage("Build-Docker-Image") {
+                    def docker_img = it["image"]["name"]
+                    def docker_tag = it["image"]["tag"]
+                    echo "docker build -t ${docker_img}:${docker_tag} ."
+                }
+                stage("Publish-Docker-Image-To-Artifactory"){
+                    def docker_arti_url = it["artifactory"]["url"]
+                    echo "Publish Docker image to ${docker_arti_url}"
+                }
+
+
             }
-        }
+            if(it["name"] == "helm" && it["chart"].containsKey("name") && it["image"].containsKey("version")){
+                stage("Build-Helm-Chart") {
+                    def helm_chart_name = it["chart"]["name"]
+                    def helm_chart_version = it["chart"]["version"]
+                    echo("Helm chart name ${helm_chart_name}")
+                    echo("Helm chart name ${helm_chart_version}")
+                    echo "Build Helm Chart ${helm_chart_name}:${helm_chart_version}"
+                }
+                stage("Publish-Helm-Chart-To-Artifactory") {
+                    def docker_arti_url = it["artifactory"]["url"]
+                    echo "Publish Helm Chart  to ${docker_arti_url}"
+                }
+            }
     }
 }
